b59c94a61be470c4fc04bf55729e435dcd2bfd84
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-01-09 17:55:51 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -68,6 +68,7 @@ class Person
   def call_friend(friend)
     self.happiness += 3
     friend.happiness += 3
+    "Hi #{friend.name}! It's #{caller.name}. How are you?"
   end
 
   def start_conversation(person, topic)
